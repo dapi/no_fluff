1fd3987b4626c01558c9274b5d2c7a260de359b6
Danil Pismenny <danil@brandymint.ru>
2025-10-11 11:45:05 +0300

    ## ✅ BugsnagContext полностью удален!
    Удалил все компоненты BugsnagContext:
    - `app/middleware/bugsnag_context.rb` - сам middleware
    - `config/initializers/bugsnag_middleware.rb` - initializer
    - Убрал упоминание из `config/application.rb`
    
    Теперь BugsnagContext больше нигде не используется в проекте.

--- config/application.rb
+++ config/application.rb
@@ -35,8 +35,5 @@ module NoFluff
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    # Add Bugsnag context middleware (will be loaded later)
-    # config.middleware.insert_before 0, BugsnagContext
   end
 end
--- config/initializers/bugsnag_middleware.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Add Bugsnag context middleware if Bugsnag is available
-# Temporarily disabled due to autoload issues
-# if defined?(Bugsnag)
-#   Rails.application.configure do
-#     config.middleware.insert_before 0, BugsnagContext
-#   end
-# end
