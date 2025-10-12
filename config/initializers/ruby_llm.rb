4b54a5fc86ee2b180dfbfa138c4deab6f57ab770
Danil Pismenny <danil@brandymint.ru>
2025-10-12 16:07:07 +0300

    Minor fix ruby_llm

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,6 +1,6 @@
 # Не настраиваем RubyLLM в тестах
 RubyLLM.configure do |config|
-  unless Rails.env.test?
+  unless Rails.env.test? || ENV.key?('SECRET_KEY_BASE_DUMMY')
     config.openai_api_key = ApplicationConfig.openai_api_key
     config.deepseek_api_key = ApplicationConfig.deepseek_api_key
     config.default_model = ApplicationConfig.llm_default_model
@@ -9,4 +9,4 @@ RubyLLM.configure do |config|
 
   # Use the new association-based acts_as API (recommended)
   config.use_new_acts_as = true
-end unless ENV.key? 'SECRET_KEY_BASE_DUMMY'
+end
--- db/migrate/20251006_remove_priority_from_subscriptions.rb
+++ db/migrate/20251006_remove_priority_from_subscriptions.rb
@@ -2,9 +2,6 @@ class RemovePriorityFromSubscriptions < ActiveRecord::Migration[8.0]
   def up
     # Удаляем колонку priority
     remove_column :subscriptions, :priority
-
-    # Удаляем индекс по полю priority если он существует
-    remove_index :subscriptions, :priority
   end
 
   def down
